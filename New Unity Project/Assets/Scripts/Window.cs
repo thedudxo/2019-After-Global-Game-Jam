1a6599e636ba0e7ed96ace5f387a4a12f1130baa
thedudxo <Isaacboon@gmail.com>
2019-01-30 16:40:11 +1300

    Lunch

---
+++ New Unity Project/Assets/Scripts/Window.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Window : MonoBehaviour { 
+
+    public float randomCloseDelay = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnMouseDrag()
+    {
+            gameObject.transform.position = Input.mousePosition;
+
+    }
+
+    public void closeWindow()
+    {
+        Destroy(gameObject, Random.Range(0.1f,randomCloseDelay));
+    }
+
+    public void dropDownMenu()
+    {
+
+    }
+}
